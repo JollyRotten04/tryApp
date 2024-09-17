986fc63eccb20257bcbe4124d19a45f3626c80a2
JollyRotten04 <vincejustinebas2004@gmail.com>
2024-09-17 10:54:17 +0800

    Second commit

---
+++ resources/views/layout1.blade.php
@@ -0,0 +1,37 @@
+<div id="firstRow">
+    <div class="vacant">ROOM 1<br>(Vacant)</div>
+    <div class="vacant">ROOM 2<br>(Vacant)</div>
+    <div class="occupied">ROOM 3<br>(Occupied)</div>
+    <div class="vacant">ROOM 4<br>(Vacant)</div>
+    <div class="occupied">ROOM 5<br>(Occupied)</div>
+    <div class="occupied">ROOM 6<br>(Occupied)</div>
+</div>
+
+<div id="secondRow">
+     <div class="occupied">ROOM 7<br>(Occupied)</div>
+    <div class="vacant">ROOM 8<br>(Vacant)</div>
+    <div class="occupied">ROOM 9<br>(Occupied)</div>
+    <div class="occupied">ROOM 10<br>(Occupied)</div>
+    <div class="vacant">ROOM 11<br>(Vacant)</div>
+    <div class="occupied">ROOM 12<br>(Occupied)</div>
+</div>
+
+<div id="thirdRow">
+    <div class="vacant">ROOM 13<br>(Vacant)</div>
+    <div class="occupied">ROOM 14<br>(Occupied)</div>
+    <div class="vacant">ROOM 15<br>(Vacant)</div>
+    <div class="vacant">ROOM 16<br>(Vacant)</div>
+    <div class="occupied">ROOM 17<br>(Occupied)</div>
+    <div class="vacant">ROOM 18<br>(Vacant)</div>
+</div>
+
+<div id="fourthRow">
+    <div class="occupied">ROOM 19<br>(Occupied)</div>
+    <div class="vacant">ROOM 20<br>(Vacant)</div>
+    <div class="vacant">ROOM 21<br>(Vacant)</div>
+    <div class="occupied">ROOM 22<br>(Occupied)</div>
+    <div class="occupied">ROOM 23<br>(Occupied)</div>
+    <div class="vacant">ROOM 24<br>(Vacant)</div>
+</div>
+
+<div 
